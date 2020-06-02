fe0c0d2f46a685c0cab9584b9eecd1be3b9ef279
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-02 16:17:34 -0700

    edited html, css and js and fixed links to bootstrap
     Co-authored-by: Chris Brauch<cmbrauch00@gmail.com>, Oscar Riley <osriley23@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){ 
+  
+
+  $("div#javascript").click(function() { 
+    console.log("test")
+    // hide all definitions 
+
+    // show specific definition you want to show
+
+    // $("body").removeclass(); 
+    // $("body").addClass("Javascript-def"); 
+   }); 
+
+
+  // $("button#Operators").click(function() { 
+  //   $("body").removeclass(); 
+  //   $("body").addClass("Operators-def"); 
+  // }); 
+});
